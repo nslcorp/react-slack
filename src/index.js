6268a7ce5f73406316a31de070eb475c3129138f
Serhii <botodnepr@gmail.com>
2019-03-26 17:56:47 +0200

    add Channel functionality

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ import { createStore } from "redux";
 import { Provider, connect } from "react-redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from "./reducers";
-import { setUser } from "./actions";
+import { setUser, clearUser } from "./actions";
 
 const store = createStore(rootReducer, composeWithDevTools());
 
@@ -58,7 +58,7 @@ const mapStateFromProps = state => ({
 const RootWithAuth = withRouter(
   connect(
     mapStateFromProps,
-    { setUser }
+    { setUser, clearUser }
   )(Root)
 );
 
